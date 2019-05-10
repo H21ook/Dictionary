5d3e61021b853367c034465f0acc205073c38b8c
H21ook <hishigbayr_3@yahoo.com>
2019-05-10 23:32:13 +0800

    search

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { DatabaseService } from '../services/database.service';
 import { Word } from '../models/word.model';
 import { IonInfiniteScroll, ToastController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-list',
@@ -10,53 +11,98 @@ import { IonInfiniteScroll, ToastController } from '@ionic/angular';
 })
 export class ListPage implements OnInit {
 
-  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-  words: Word[] = [];
-  displayWords: Word[] = [];
+  words: Word[] = [];         //Oorchlogdohgui
+  libWords: Word[] = [];      //Hailt hiiwel hailtiin ilerts awah zereg oorchlogdono
+  displayWords: Word[] = [];  //delgetsend haragdah ugnuud
+  LIMIT = 20;                 //neg udaa unshih hyzgaar
   word: Word;
   index: number = 0;
+  searchField: string = "eng";  //hailt hiih talbar
+  
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   constructor(
     private db: DatabaseService,
-    private toastController: ToastController
+    private toastController: ToastController,
+    private splashScreen: SplashScreen
   ) { }
 
   ngOnInit() {
+    this.infiniteScroll.disabled = true;
     this.db.getDatabaseState().subscribe(ready => {
       if (ready) {
         this.db.getWords().subscribe(words => {
+          this.splashScreen.hide();
           this.words = words;
+          this.getDefaultValue();
         })
       }
     });
   }
 
+  getDefaultValue() {
+    this.libWords = this.words;
+    this.index = 0;
+    this.displayWords = this.words.slice(this.index, this.index + this.LIMIT);
+    this.index += this.LIMIT;
+    if(this.libWords.length > 20) {
+      this.infiniteScroll.disabled = false;
+    }
+  }
+
   loadData(event) {
     setTimeout(() => {
-      this.presentToast('Done');
-      event.target.complete();
-
-      if (this.words.length == 1000) {
+        event.target.complete();
+      if(this.libWords.length > 0 && this.index <= this.libWords.length) {
+        this.getPaginateData();
+        this.index += this.LIMIT;
+      } else {
+        if((this.index - this.libWords.length) > 0 && this.libWords.length > 0) {
+          this.getPaginateData();
+        }
         event.target.disabled = true;
       }
-    });
+    }, 1000);
   }
 
   getPaginateData() {
-    if(this.words.length > 0 && this.index < this.words.length) {
-      this.displayWords = this.words.slice(this.index + 20);
-    }
-  }
-  toggleInfiniteScroll() {
-    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+    this.displayWords = this.displayWords.concat(this.libWords.slice(this.index, this.index + this.LIMIT));
   }
   
   async presentToast(msg: string) {
     const toast = await this.toastController.create({
       message: msg,
+      position: 'top',
       duration: 2000
     });
     toast.present();
+  }
+
+  search(e) {
+    var search = e.target.value;
+    this.infiniteScroll.disabled = true;
+    this.index = 0;
+    this.displayWords = [];
+    this.libWords = [];
+
+    if (!search) {
+      this.getDefaultValue();
+      this.presentToast("hooson");
+    } else {
+      this.words.filter(data => {
+        if (data[this.searchField].trim().toLowerCase().includes(search.trim().toLowerCase())) {
+          this.libWords.push(data);
+        }
+      });
+      this.presentToast("lib urt: " + this.libWords.length);
+      this.displayWords = this.libWords.slice(this.index, this.index + this.LIMIT);
+      this.index += this.LIMIT;
+
+      if(this.libWords.length > 20) {
+        this.infiniteScroll.disabled = false;
+      } 
+      
+    }
   }
   /*readData() {
     if(this.words.length <= 0) {
