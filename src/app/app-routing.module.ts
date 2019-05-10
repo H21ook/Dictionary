c67d20af6a8a9117fed2bf95600186bb61dbba53
H21ook <hishigbayr_3@yahoo.com>
2019-05-11 01:51:02 +0800

    zaswar detail pagetei holbootoi bas design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'detail/:id', loadChildren: './detail/detail.module#DetailPageModule' }
 ];
 
 @NgModule({
